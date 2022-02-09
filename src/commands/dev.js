b47f1b3e262e45fce03165dc3c9a3998a3fda9a3
BitRapture <thinzy.business+bitrapture@gmail.com>
2022-02-09 18:21:31 +0000

    Add back important commands

---
+++ src/commands/dev.js
@@ -0,0 +1,34 @@
+
+module.exports = {
+    name : "dev",
+    desc : "The developer interface",
+    longdesc : "This command is supposed to be invisible >:(",
+    examples : [
+        { name: "You get NO EXAMPLES", value: "`Get owned!`" }
+    ],
+    visible : false,
+
+    Run(Bot, args, message) {
+        if (message.author.id !== Bot.config.devId) { return; }
+        if (args.length <= 0) { return; }
+        // Run through developer commands
+        switch (args[0].toLowerCase())
+        {
+            case "push":
+            case "up":
+            case "update":
+                Bot.store.dev.set("update", { recent: true, issuer: `${message.author}`, channel: message.channel.id, guild: message.guildId });
+                process.exit(2);
+            break;
+            case "eval":
+                args.shift();
+                eval(args.join(" "));
+            break;
+            case "dellvl":
+                Bot.store.users.exp.forEach((v, k) => {Bot.store.users.exp.delete(k)});
+                Bot.store.users.lvl.forEach((v, k) => {Bot.store.users.lvl.delete(k)});
+                message.reply("Deleted everything");
+            break;
+        }
+    }
+}
