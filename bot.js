41a45740bdaf5231ca2f902653ec0e161248f102
BitRapture <thinzy.business+bitrapture@gmail.com>
2022-02-09 17:42:53 +0000

    Purge to main framework

--- bot.js
+++ bot.js
@@ -12,21 +12,7 @@ Bot.config = require("./src/config.json");
 // SQL storage initialization
 const Enmap = require("enmap");
 Bot.store = {
-    dev : new Enmap({ name: "dev", fetchAll: false, dataDir: "./store" }),
-    users : {
-        inv : new Enmap({ name: "userInv", fetchAll: false, dataDir: "./store" }),      // Inventory
-        lvl : new Enmap({ name: "userLvl", fetchAll: false, dataDir: "./store" }),      // Level
-        exp : new Enmap({ name: "userExp", fetchAll: false, dataDir: "./store" }),      // Experience
-        bal : new Enmap({ name: "userBal", fetchAll: false, dataDir: "./store" }),      // Balance
-        aly : new Enmap({ name: "userAly", fetchAll: false, dataDir: "./store" })       // Alliance 
-    },
-    alliances : {
-        list : new Enmap({ name: "alyList", fetchAll: false, dataDir: "./store" }),     // List of users
-        mult : new Enmap({ name: "alyMult", fetchAll: false, dataDir: "./store" }),     // Current currency multiplyer
-        lvl : new Enmap({ name: "alyLvl", fetchAll: false, dataDir: "./store" }),       // Level of alliance
-        exp : new Enmap({ name: "alyExp", fetchAll: false, dataDir: "./store" }),       // Experience of alliance
-        owner : new Enmap({ name: "alyOwner", fetchAll: false, dataDir: "./store" })    // Owner of alliance
-    }
+    dev : new Enmap({ name: "dev", fetchAll: false, dataDir: "./store" })
 };
 
 // Command manager initialization
@@ -76,14 +62,6 @@ Bot.client.on("messageCreate", (message) => {
     Bot.config.serverTargets.forEach((id) => { inServer |= (id === message.guildId); });
     if (!inServer) { return; }
     console.log(`${message.author.username}: ${message.content}`);
-
-    // Streaming handling
-    if (!message.content.startsWith(Bot.config.prefix)) { 
-        Bot.streams.forEach((stream) => {
-            stream.Run(Bot, message);
-        });
-        return;
-    }
 
     // Command handling
     let args = message.content.slice(Bot.config.prefix.length).split(" ");
