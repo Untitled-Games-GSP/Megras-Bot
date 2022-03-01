a9b6d73a51f45e3474790ad7fb7b7561c63e9a59
BitRapture <62811797+BitRapture@users.noreply.github.com>
2022-03-01 18:18:36 +0000

    Test flipped operator

--- src/commands/genlogs.js
+++ src/commands/genlogs.js
@@ -18,7 +18,7 @@ function generateDocs(gitCommits) {
     
     // Sort array by date
     allCommits.sort((a, b) => {
-        return (Date.parse(a.date) > Date.parse(b.date));
+        return (Date.parse(a.date) < Date.parse(b.date));
     });
 
     // Save all commits to docx table objects
